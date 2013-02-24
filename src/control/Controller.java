ed3fcf4786f3e87f793cb195db6e1a5501b60162
Ryan Fishel <ref13@duke.edu>
2013-02-24 13:20:38 -0500

    added a little to the Controller class
    also created the view package and View class

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -1,36 +1,60 @@
 package control;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.util.Scanner;
 
 import simulation.Model;
+import view.View;
 
 public class Controller {
-	
+
 	Model myModel;
 	View myView;
-	
-	public Controller(Model model, View view){
+
+	public Controller(Model model, View view) {
 		myModel = model;
 		myView = view;
 	}
-	
-	public Instruction sendString(String s){
+
+	public Instruction sendString(String s) {
 		return Parser.generateInstruction(s);
 	}
+
 	public void saveState(FileWriter fw){
-		
+		try{
+			BufferedWriter out = new BufferedWriter(fw);
+			// TODO: write the variables that need to be saved
+		}
+		catch(Exception e){
+			// print error
+		}
 	}
-	public void loadState(File f){
-		
+
+	public void loadState(File f) {
+		try {
+			Scanner input = new Scanner(f);
+			while (input.hasNext()) {
+				Scanner line = new Scanner(input.nextLine());
+				// TODO: create variables
+			}
+
+			input.close();
+		} catch (FileNotFoundException e) {
+			// should not happen because File came from user selection
+			e.printStackTrace();
+		}
+
 	}
-	
-	public void clear(){
-		
+
+	public void clear() {
+
 	}
-	
-	public void runInstruction(Instruction instr, Model myModel){
-		//myModel.getTurtle.perfromAction(instr);
+
+	public void runInstruction(Instruction instr, Model myModel) {
+		// myModel.getTurtle.perfromAction(instr);
 	}
-	
+
 }
