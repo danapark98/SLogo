e507493d524c2b80316c21f77592f2c028dd66ba
Sean Wareham <srwareham@hotmail.com>
2013-02-25 22:26:38 -0500

    formatting

--- src/view/Canvas.java
+++ src/view/Canvas.java
@@ -7,15 +7,16 @@ import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Set;
-
 import javax.swing.JComponent;
 import javax.swing.Timer;
-
 import simulation.Model;
+
+
 /**
  * The Canvas is the space within the view that will contain a turtle and any lines drawn by it.
+ * 
  * @author srwareham
- *
+ * 
  */
 public class Canvas extends JComponent {
     /**
@@ -26,7 +27,8 @@ public class Canvas extends JComponent {
      * Frames_Per_Second for running the simulation.
      */
     public static final int FRAMES_PER_SECOND = 30;
-    /**\
+    /**
+     * 
      * Number of milliseconds in a second.
      */
     public static final int ONE_SECOND = 1000;
@@ -42,33 +44,39 @@ public class Canvas extends JComponent {
     private int myLastKeyPressed;
     private Set<Integer> myKeys;
     private Dimension myBounds;
-/**
- * Creates a Canvas of the given size.
- * @param size Desired Dimension for the Canvas
- * @param view Parent view for this Canvas
- * @param model The simulation environment to be displayed in the canvas
- */
+
+    /**
+     * Creates a Canvas of the given size.
+     * 
+     * @param size Desired Dimension for the Canvas
+     * @param view Parent view for this Canvas
+     * @param model The simulation environment to be displayed in the canvas
+     */
     public Canvas (Dimension size, SLogoView view, Model model) {
         myView = view;
         mySimulation = model;
         myBounds = size;
         setPreferredSize(size);
         setSize(size);
-        //do  not believe this needs to be focusable because there is no user interaction
-        //with the canvas display being showed
+        // do not believe this needs to be focusable because there is no user interaction
+        // with the canvas display being showed
     }
 
-    /**\
+    /**
+     * 
      * This method paints all Graphics objects such as the turtle and the lines.
+     * 
      * @param pen 
      */
-    public void paintComponent(Graphics pen) {
+    @Override
+    public void paintComponent (Graphics pen) {
         pen.setColor(Color.WHITE);
         pen.fillRect(0, 0, getSize().width, getSize().height);
         if (mySimulation != null) {
             mySimulation.paint((Graphics2D) pen);
         }
     }
+
     /**
      * Starts the timer which is responsible for updating the model.
      * This is within the purview of the View because at every frame the view
@@ -76,32 +84,35 @@ public class Canvas extends JComponent {
      */
     public void start () {
         // create a timer to animate the canvas
-        myTimer = new Timer(DEFAULT_DELAY, 
-            new ActionListener() {
-                public void actionPerformed (ActionEvent e) {
-                    step(mySimulation);
-                }
-            });
+        myTimer = new Timer(DEFAULT_DELAY,
+                            new ActionListener() {
+                    @Override
+                    public void actionPerformed (ActionEvent e) {
+                        step(mySimulation);
+                    }
+                });
         // start animation
-//        mySimulation = new Model(myView);
-//        loadModel();
+        // mySimulation = new Model(myView);
+        // loadModel();
         myTimer.start();
     }
-    
-    //TODO: we will need to add stop to the api
+
+    // TODO: we will need to add stop to the api
     /**
      * Stops the timer animating the simulation.
      */
-    public void stop() {
+    public void stop () {
         myTimer.stop();
     }
-    //TODO: we will need to add step to the api might be able to keep this private
+
+    // TODO: we will need to add step to the api might be able to keep this private
     /**
      * Increments the animation one step.
+     * 
      * @param simulation The model to be stepped
      */
     public void step (Model simulation) {
-        simulation.update((double)FRAMES_PER_SECOND / ONE_SECOND, myBounds);
+        simulation.update((double) FRAMES_PER_SECOND / ONE_SECOND, myBounds);
         repaint();
     }
 }
--- src/view/View.java
+++ src/view/View.java
@@ -7,111 +7,120 @@ import java.awt.event.FocusListener;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
-
 import control.Controller;
 
 
 /**
  * The View is comprised of everything visible to the user.
  * The View is the interactive space containing a file menu, buttons, and the Canvas.
+ * 
  * @author srwareham
- *
+ * 
  */
 public class View extends SLogoView {
     private static final String USER_DIR = "user.dir";
     public static final String FORWARD = "ForwardCommand";
     public static final String FD = "fd ";
     public static final int DEFAULT_FD_MAG = 10;
-    private JFileChooser myChooser;private ActionListener myActionListener;
+    private JFileChooser myChooser;
+    private ActionListener myActionListener;
     private KeyListener myKeyListener;
     private MouseListener myMouseListener;
     private MouseMotionListener myMouseMotionListener;
     private FocusListener myFocusListener;
-   
-/**
- * Creates an instance of the View.
- * @param title The title of this View
- * @param language The desired language for the View
- */
-    public View(String title, String language) {
+
+    /**
+     * Creates an instance of the View.
+     * 
+     * @param title The title of this View
+     * @param language The desired language for the View
+     */
+    public View (String title, String language) {
         super(title, language);
         getContentPane().add(makeInput(), BorderLayout.NORTH);
-//        getContentPane().add(myCanvas, BorderLayout.CENTER);
+        // getContentPane().add(myCanvas, BorderLayout.CENTER);
         pack();
         setVisible(true);
-        
+
     }
 
     @Override
-    public void displayText(String text) {
+    public void displayText (String text) {
 
     }
+
     @Override
-    protected JComponent makeInput() {
+    protected JComponent makeInput () {
         JPanel result = new JPanel();
         result.add(makeForwardButton());
         return result;
     }
-    
-    @Override protected JComponent makeDisplay() {
+
+    @Override
+    protected JComponent makeDisplay () {
         JPanel panel = new JPanel();
         panel.add(myCanvas);
         return panel;
     }
-    
-    private JButton makeForwardButton() {
-        //TODO: change fd mag to a variable from an input slider
-        //we also need to look into this final usage
+
+    private JButton makeForwardButton () {
+        // TODO: change fd mag to a variable from an input slider
+        // we also need to look into this final usage
         final String command = FD + DEFAULT_FD_MAG;
         final Controller controller = super.myController;
         JButton button = new JButton(super.myResources.getString(FORWARD));
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed (ActionEvent e) {
-                //TODO: this was a change to the API we noticed
+                // TODO: this was a change to the API we noticed
                 controller.createRunInstruction(command);
                 System.out.println("Forward Button");
             }
         });
         return button;
     }
-    
-    
-    
-//    myController.sendString(s);
 
-    
-    //TODO: we may add addJComponent(JComponent j) to our controller so that it can recieve instances of swing objects so that it can use "j.addKeyListener( new .....)" would need to document as a change to our API  
-    //the issue stems from the fact that the view holds the JComponent, while the Control hods the definiton for what the actionPerformed should be.  May also need seperate one for JButton to be able to addActionListener for some reason
+    // myController.sendString(s);
+
+    // TODO: we may add addJComponent(JComponent j) to our controller so that it can recieve
+    // instances of swing objects so that it can use "j.addKeyListener( new .....)" would need to
+    // document as a change to our API
+    // the issue stems from the fact that the view holds the JComponent, while the Control hods the
+    // definiton for what the actionPerformed should be. May also need seperate one for JButton to
+    // be able to addActionListener for some reason
     /*
-     * Example: 
-     *  protected JButton makeClear () {
-        JButton result = new JButton(myResources.getString("ClearCommand"));
-        result.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed (ActionEvent e) {
-                myTextArea.setText("");
-            }
-        });
-        return result;
-    }
-    
-    probably we want to send the button or any other componenet type (not sure if we want several specific methods or 1 more general one) and then add the listener and action performed to it 
-    
-    otherwise stuck wondering if there is a way to pass off a JButton with an ActionListener to the control class. Then have the control class override the actionPerformed so that it does what control knows it needs to
-    suppose we could pass off the JComponents with as much info as possible from the view. Then let the controller read all of its Listener info, create a new instance with the proper actionPerformed, and then 
-    place this newer ActionListener w/ complete actionPerformed into the JButton
-    
+     * Example:
+     * protected JButton makeClear () {
+     * JButton result = new JButton(myResources.getString("ClearCommand"));
+     * result.addActionListener(new ActionListener() {
+     * 
+     * @Override
+     * public void actionPerformed (ActionEvent e) {
+     * myTextArea.setText("");
+     * }
+     * });
+     * return result;
+     * }
+     * 
+     * probably we want to send the button or any other componenet type (not sure if we want several
+     * specific methods or 1 more general one) and then add the listener and action performed to it
+     * 
+     * otherwise stuck wondering if there is a way to pass off a JButton with an ActionListener to
+     * the control class. Then have the control class override the actionPerformed so that it does
+     * what control knows it needs to
+     * suppose we could pass off the JComponents with as much info as possible from the view. Then
+     * let the controller read all of its Listener info, create a new instance with the proper
+     * actionPerformed, and then
+     * place this newer ActionListener w/ complete actionPerformed into the JButton
      */
 
-public static void main(String[] args){
-    SLogoView hi = new View("HI","English");
+    public static void main (String[] args) {
+        new View("HI", "English");
 
-}
+    }
 
 }
