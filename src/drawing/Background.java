cae3b71167f99c29befcfffed82801cb019875e3
Scott Valentine <scott.douglas.valentine@gmail.com>
2013-03-20 20:58:07 -0400

    started background class

---
+++ src/drawing/Background.java
@@ -0,0 +1,37 @@
+package drawing;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import util.Pixmap;
+
+public class Background {
+    //private Collection<Pixmap> myBackgroundImages;
+    
+    private Map<String,Collection<Pixmap>> myUtilities;
+    //private Collection<Pixmap> myActiveBackgrounds;
+    
+    public Background() {
+        initializeImages();
+        
+    }
+    
+    public setBackground(int index) {
+        if (index)
+    }
+ 
+    private void initializeImages() {
+        myUtilities = new HashMap<String, Collection<Pixmap>>();
+        
+        Collection<Pixmap> grids = new ArrayList<Pixmap>();
+        
+        Pixmap grid = 
+        
+        grids.add(grid);
+        
+        myUtilities.put("grid", grids);
+        myUtilities.
+    }
+    
+}
