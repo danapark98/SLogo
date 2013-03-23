8193e36e66d3bbc1845ee16a53bcfbc8593eedbf
Ryan Fishel <ref13@duke.edu>
2013-03-23 15:01:14 -0400

    shapes

---
+++ src/instructions/turtle/Shape.java
@@ -0,0 +1,37 @@
+package instructions.turtle;
+
+import simulation.Model;
+import exceptions.IllegalInstructionException;
+import instructions.BaseInstruction;
+
+/**
+ * Returns the index of the current shape
+ * 
+ * @author Scott Valentine
+ * @author Ryan Fishel
+ * @author Ellango Jothimurugesan
+ * 
+ *
+ */
+public class Shape extends BaseInstruction{
+
+	
+    /**
+	 * Eclipse auto-generated ID to implement Serializable interface.
+	 */
+	private static final long serialVersionUID = -5707107616926115718L;
+	private static final int NUMBER_OF_ARGUMENTS = 0;
+    
+    /**
+     * Initializes a shape instruction
+     */
+    public Shape() {
+    	setNumberOfArguments(NUMBER_OF_ARGUMENTS);
+    }
+    
+	@Override
+	public int execute(Model model) throws IllegalInstructionException {
+		return model.getPalette().getCurrentImageIndex();
+	}
+
+}
