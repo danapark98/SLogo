58d200dc888f446da80b0dae396b2253c2333528
Ryan Fishel <ref13@duke.edu>
2013-03-23 15:15:11 -0400

    added comments

--- src/drawing/ImagePalette.java
+++ src/drawing/ImagePalette.java
@@ -24,14 +24,19 @@ public class ImagePalette {
     public static final int SQUIRREL_IMAGE_INDEX = 2;
     public static final int UNICORN_IMAGE_INDEX = 3;
 
-  
-
     private Map<Integer, Pixmap> myImages;
 
+    /**
+     * creates a new image palette that initializes the map of integers to pixmaps
+     */
     public ImagePalette () {
         initializeImageMap();
     }
 
+    /**
+     * Creates a hashmap of Integer to Pixmap and adds all of the images for the turtle
+     * to the map
+     */
     private void initializeImageMap () {
     	myImages = new HashMap<Integer, Pixmap>();
         myImages.put(DEFAULT_IMAGE_INDEX, DEFAULT_IMAGE);
@@ -41,6 +46,13 @@ public class ImagePalette {
         myImages.put(UNICORN_IMAGE_INDEX, new Pixmap("Unicorn.jpg"));
     }
 
+    /**
+     * Returns the image that the turtle should be given an input index.
+     * @param index is the index of the image that is wanted in the image map
+     * @return the image that the user wants the turtle to be
+     * @throws IllegalInstructionException occurs when the index the user
+     * asks for is not contained in the image map.
+     */
     public Pixmap getImage (int index) throws IllegalInstructionException {
         if (!myImages.containsKey(index)) {
         throw new IllegalInstructionException(Palette.UNDEFINED_INDEX_MESSAGE + index);
