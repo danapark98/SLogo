3f3f3ad66bb1a2ad0ea951dd8f9e25b4b624bb44
Yoshi <elder.yoshida@gmail.com>
2013-03-25 04:21:54 -0400

    Clickable History command lines (some bugs)
    The scroll pane is not scrollig correctly….

--- src/view/SLogoView.java
+++ src/view/SLogoView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,8 +15,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
+import javax.swing.text.DefaultEditorKit;
+import javax.swing.text.StyledDocument;
 
 
 /**
@@ -119,7 +123,9 @@ public class SLogoView extends View {
      * the controller messages
      */
     private JTextArea myHistory;
-
+    private JPanel myHistoryPanel;
+    private JPanel histPane;
+    
     /**
      * Creates an instance of the View with personalized layout.
      * 
@@ -132,7 +138,7 @@ public class SLogoView extends View {
         pack();
         setVisible(true);
     }
-
+    
     /**
      * Displays the text in the History panel and appends any text to it
      * 
@@ -140,13 +146,36 @@ public class SLogoView extends View {
      */
     @Override
     public void displayText (String text) {
-        String s;
-        if (text.length() > 0) {
-            s = (myHistory.getText().length() == 0) ? text : "\n" + text;
-            myHistory.append(s);
-        }
+        /*
+         * String s;
+         * if (text.length() > 0) {
+         * s = (myHistory.getText().length() == 0) ? text : "\n"+ text;
+         * myHistory.append(s);
+         * }
+         */
+        
+        myHistoryPanel.add(makeTextButton(text));
+        myHistoryPanel.revalidate();
+        histPane.revalidate();
+        
     }
-
+    
+    private JButton makeTextButton (final String text) {
+        JButton button = new JButton(text);
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+        button.setBorderPainted(false);
+        if (!text.startsWith(">>")) {
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed (ActionEvent e) {
+                    submitCommand(text);
+                }
+            });
+        }
+        return button;
+    }
+    
     /**
      * Creates the main panel with a Canvas, a History and an Input panels.
      * 
@@ -158,7 +187,7 @@ public class SLogoView extends View {
         workspace.addTab(getResourceName(WORKSPACE_NAME), null, contentPanel, SLOGO_NAME);
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.LINE_AXIS));
         contentPanel.setBorder(makeBorder(""));
-
+        
         JPanel hstInpPanel = new JPanel();
         hstInpPanel.setLayout(new BoxLayout(hstInpPanel, BoxLayout.PAGE_AXIS));
         hstInpPanel.add(makeInput());
@@ -167,7 +196,7 @@ public class SLogoView extends View {
         contentPanel.add(hstInpPanel);
         return workspace;
     }
-
+    
     /**
      * Creates a panel with the Canvas.
      * 
@@ -180,26 +209,33 @@ public class SLogoView extends View {
         canvasPanel.setBorder(makeBorder(CANVAS_NAME));
         return canvasPanel;
     }
-
+    
     /**
      * Creates a panel with the History, where the commands and results from the user are recorded.
      * 
      * @return Panel with History.
      */
     private JPanel makeHistoryPanel () {
-        JPanel histPane = new JPanel();
-        JTextArea textArea = new JTextArea();
-        myHistory = textArea;
-        textArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-
-        scrollPane.setPreferredSize(PREFERRED_HISTORY_SIZE);
+        histPane = new JPanel();
+        /*
+         * JTextArea textArea = new JTextArea();
+         * myHistory = textArea;
+         * textArea.setEditable(false);
+         */
+        
+        myHistoryPanel = new JPanel();
+        myHistoryPanel.setPreferredSize(PREFERRED_HISTORY_SIZE);
+        myHistoryPanel.setBackground(Color.white);
+        myHistoryPanel.setLayout(new BoxLayout(myHistoryPanel, BoxLayout.PAGE_AXIS));
+        // myHistoryPanel.getDocument().putProperty(DefaultEditorKit.EndOfLineStringProperty, "\n");
         histPane.setLayout(new BoxLayout(histPane, BoxLayout.PAGE_AXIS));
-        histPane.add(scrollPane);
+        histPane.setPreferredSize(PREFERRED_HISTORY_SIZE);
+        JScrollPane scroller = new JScrollPane(myHistoryPanel);
+        histPane.add(scroller);
         histPane.setBorder(makeBorder(HISTORY_NAME));
         return histPane;
     }
-
+    
     /**
      * Creates the input panel with buttons for the user to interact with the turtle
      * and the console in which the user writes the SLogo code.
@@ -216,7 +252,7 @@ public class SLogoView extends View {
         result.add(makeSubmitButton(), BorderLayout.SOUTH);
         return result;
     }
-
+    
     /**
      * Creates the buttons for moving the turtle.
      * The layout is not very nice, right now and are going to be improved in the following version.
@@ -229,7 +265,7 @@ public class SLogoView extends View {
                 new ImageIcon(getClass().getResource(RESOURCE_LOCATION + TURTLE_IMG_FILENAME));
         JLabel label = new JLabel(icon);
         turtleMovePane.setLayout(new BorderLayout());
-
+        
         JButton fdButton =
                 makeTurtleMoveButton(getResourceName(FORWARD_LABEL), getResourceName(FD_COMMAND) +
                                                                      " " + DEFAULT_FD_MAG);
@@ -242,16 +278,16 @@ public class SLogoView extends View {
         JButton backButton =
                 makeTurtleMoveButton(getResourceName(BACKWARD_LABEL), getResourceName(FD_COMMAND) +
                                                                       " " + -DEFAULT_FD_MAG);
-
+        
         turtleMovePane.add(fdButton, BorderLayout.NORTH);
         turtleMovePane.add(leftButton, BorderLayout.LINE_START);
         turtleMovePane.add(rightButton, BorderLayout.LINE_END);
         turtleMovePane.add(backButton, BorderLayout.SOUTH);
         turtleMovePane.add(label, BorderLayout.CENTER);
-
+        
         return turtleMovePane;
     }
-
+    
     /**
      * Creates a button that moves the turtle forward.
      * 
@@ -264,7 +300,7 @@ public class SLogoView extends View {
         pane.setPreferredSize(PREFERRED_CONSOLE_SIZE);
         return pane;
     }
-
+    
     /**
      * Creates a button that submits the text in the console to the controller.
      * The code for this button is very similar to the code of the makeTurtleMoveButton method
@@ -277,14 +313,14 @@ public class SLogoView extends View {
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed (ActionEvent e) {
-                submitControllCommand(myConsole.getText());
+                submitCommand(myConsole.getText());
                 myConsole.setText("");
             }
         });
         submitButton.setFocusable(false);
         return submitButton;
     }
-
+    
     /**
      * Creates a button that takes in a final command.
      * Used to create the turtle movement buttons.
@@ -296,29 +332,29 @@ public class SLogoView extends View {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed (ActionEvent e) {
-                submitControllCommand(command);
+                submitCommand(command);
             }
         });
         button.setFocusable(false);
         return button;
     }
-
+    
     /**
      * Sends the string command for the controller to parse/ process/
      * 
      * @param command
      */
-    private void submitControllCommand (String command) {
+    private void submitCommand (String command) {
         super.getController().createRunInstruction(command);
     }
-
+    
     /**
      * Creates a default border for most of the panels.
      * 
      * @return border for panels
      */
     private Border makeBorder (String panelName) {
-
+        
         Border border;
         String title = ("".equals(panelName)) ? "" : getResourceName(panelName);
         TitledBorder titleBorder = BorderFactory.createTitledBorder(title);
@@ -327,7 +363,7 @@ public class SLogoView extends View {
         border = BorderFactory.createCompoundBorder(titleBorder, emptyBorder);
         return border;
     }
-
+    
     /**
      * Returns a string with the text from the language resource file.
      * If there are multiple the first is returned.
